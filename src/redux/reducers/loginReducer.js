ef3243ee23ba40a56679b6f2da1d6b377ae689b6
Rodrigo Sakae <devsakae@gmail.com>
2022-10-07 15:39:30 -0300

    [Bug fix] Ajustes no reducer

--- src/redux/reducers/loginReducer.js
+++ src/redux/reducers/loginReducer.js
@@ -1,13 +1,16 @@
 import { USER_LOGIN } from '../actions';
 
-const INITIAL_STATE = { email: '', name: '' };
+const INITIAL_STATE = {
+  email: '',
+  name: '',
+};
 
 const loginReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case USER_LOGIN:
     return { ...state, ...action.payload };
   default:
-    break;
+    return state;
   }
 };
 
