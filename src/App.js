55785756ffa1061f0b3c09b9c4af0a58993da123
Ranilson Baldez <ranilson12@gmail.com>
2022-10-07 18:13:46 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
+import Game from './pages/Game';
 import './App.css';
 
 export default function App() {
@@ -9,6 +9,7 @@ export default function App() {
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route exact path="/settings" component={ Settings } />
+      <Route exact path="/game" component={ Game } />
     </Switch>
   );
 }
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,3 +1,9 @@
 export const USER_LOGIN = 'USER_LOGIN';
+export const SET_SCORE = 'SET_SCORE';
 
 export const userLogin = (payload) => ({ type: USER_LOGIN, payload });
+
+export const setScore = (score) => ({
+  type: SET_SCORE,
+  score,
+});
