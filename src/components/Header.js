d7048ce4ef0a533258d997ec51f873da58b15dab
Rodrigo Sakae <devsakae@gmail.com>
2022-10-11 09:19:33 -0300

    [Bug fixes] Header mostra a pontuação

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,29 +5,37 @@ import { MD5 } from 'crypto-js';
 
 class Header extends Component {
   render() {
-    const { nome, email } = this.props;
+    const { nome, email, score } = this.props;
     return (
-      <>
-        <img
-          data-testid="header-profile-picture"
-          src={ `https://www.gravatar.com/avatar/${MD5(email).toString()}` }
-          alt="profile"
-        />
-        <p data-testid="header-player-name">{ nome }</p>
-        <p data-testid="header-score">Placar: 0</p>
-      </>
+      <header>
+        <div>
+          <img
+            data-testid="header-profile-picture"
+            src={ `https://www.gravatar.com/avatar/${MD5(email).toString()}` }
+            alt="profile"
+          />
+          <p data-testid="header-player-name">{ nome }</p>
+        </div>
+        <div>
+          <h1>Trivia MG25</h1>
+        </div>
+        <div>
+          <p>Sua pontuação:</p>
+          <p data-testid="header-score">{ score }</p>
+        </div>
+      </header>
     );
   }
 }
 
 Header.propTypes = {
-  nome: PropTypes.string.isRequired,
-  email: PropTypes.string.isRequired,
-};
+  nome: PropTypes.string,
+  email: PropTypes.string,
+  score: PropTypes.number,
+}.isRequired;
 
 const mapStateToProps = (state) => ({
-  nome: state.player.nome,
-  email: state.player.email,
+  ...state.player,
 });
 
 export default connect(mapStateToProps)(Header);
