c9c3d2023f6b0d28a43a7ca471336190a9232cea
Victor Hugo <victor.hiraiftw@outlook.com>
2022-10-13 15:14:10 -0300

    req 4 resolvido

--- src/tests/01.Login.test.js
+++ src/tests/01.Login.test.js
@@ -11,13 +11,13 @@ describe('Testes de login', () => {
   test('Verifica os campos de login nome são renderizados e é possivel digitar', () => {
     renderWithRouterAndRedux(<Login />);
 
-    const inputName = screen.getByPlaceholderText(/digite seu nome/i);
+    const inputName = screen.getByPlaceholderText(/nome do jogador/i);
     expect(inputName).toBeInTheDocument();
 
     const inputEmail = screen.getByPlaceholderText(/digite seu e-mail/i);
     expect(inputEmail).toBeInTheDocument();
 
-    const playButton = screen.getByRole('button', { name: /play/i });
+    const playButton = screen.getByRole('button', { name: /jogar/i });
     expect(playButton).toBeInTheDocument();
     expect(playButton).toBeDisabled();
 
@@ -25,7 +25,7 @@ describe('Testes de login', () => {
     userEvent.type(inputEmail, loginMock.email);
     expect(playButton).toBeEnabled();
 
-    const buttonSettings = screen.getByRole('button', { name: /settings/i });
+    const buttonSettings = screen.getByRole('button', { name: /configurações/i });
     expect(buttonSettings).toBeInTheDocument();
   });
 });
@@ -35,7 +35,7 @@ describe('Testes do App', () => {
     const { history } = renderWithRouterAndRedux(<App />);
     expect(history.location.pathname).toBe('/');
 
-    const buttonSettings = screen.getByRole('button', { name: /settings/i });
+    const buttonSettings = screen.getByRole('button', { name: /configurações/i });
     userEvent.click(buttonSettings);
 
     const settingsText = await screen.findByRole('heading', {
@@ -47,9 +47,9 @@ describe('Testes do App', () => {
   test('Teste da rota de game', async () => {
     const { history } = renderWithRouterAndRedux(<App />);
 
-    const inputName = screen.getByPlaceholderText(/digite seu nome/i);
+    const inputName = screen.getByPlaceholderText(/nome do jogador/i);
     const inputEmail = screen.getByPlaceholderText(/digite seu e-mail/i);
-    const playButton = screen.getByRole('button', { name: /play/i });
+    const playButton = screen.getByRole('button', { name: /jogar/i });
 
     userEvent.type(inputName, loginMock.name);
     userEvent.type(inputEmail, loginMock.email);
@@ -57,7 +57,7 @@ describe('Testes do App', () => {
 
     const name = await screen.findByText(loginMock.name);
     const img = screen.getByRole('img', { name: /profile/i });
-    const placar = screen.getByText(/placar:/i);
+    const placar = screen.getByText(/sua pontuação:/i);
 
     expect(placar).toBeInTheDocument();
     expect(name).toBeInTheDocument();
