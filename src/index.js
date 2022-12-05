505f1f0d4d2fad2362bc353429969e5cf283b01f
devsakae <devsakae@gmail.com>
2022-12-05 18:02:27 -0300

    feat: gh-pages settings accordingly browser router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
@@ -8,8 +8,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <Provider store={ store }>
-      <App />
-    </Provider>
+    <HashRouter basename="/">
+      <Provider store={ store }>
+        <App />
+      </Provider>
+    </HashRouter>
   </BrowserRouter>,
 );
